af4e3e4bf8af98a7cb560a54e443da6fd2152019
Coutego <coutego@gmail.com>
2021-12-19 00:44:25 +0100

    day 3 part b

--- src/aoc_2021/core.clj
+++ src/aoc_2021/core.clj
@@ -12,6 +12,7 @@
        (filter true?)
        count))
 
+
 ;;; Day two
 
 (defn parse [commands]
@@ -60,3 +61,23 @@
 (def gamma (partial calc true))
 (def epsilon (partial calc false))
 (defn power [bnums] (* (gamma bnums) (epsilon bnums)))
+
+(defn- retain [most-common? ind bnums]
+  (let [it (group-by #(nth % ind) bnums)
+        it (if ((if most-common? < >=)
+                (count (it \1))
+                (count (it \0)))
+             (it \1)
+             (it \0))]
+    (if (= 1 (count it))
+      it
+      (recur most-common? (inc ind) it))))
+
+(defn bstr2num [s]
+  (reduce #(+ (* 2 %1) (Integer/parseInt (str %2))) 0 s))
+
+(def oxigen-r (comp bstr2num first (partial #'retain true 0) str/split-lines))
+(def co2-sc-r (comp bstr2num first (partial #'retain false 0) str/split-lines))
+
+(defn life-support-rating [bnums]
+  (* (oxigen-r bnums) (co2-sc-r bnums)))
