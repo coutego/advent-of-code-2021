b048d64b8cdb96e4f78f4c7c11475cd14d46f200
Coutego <coutego@gmail.com>
2021-12-18 22:55:51 +0100

    refactor day2 code

--- src/aoc_2021/core.clj
+++ src/aoc_2021/core.clj
@@ -17,29 +17,22 @@
 (defn parse [commands]
   (->> commands
        str/split-lines
-       (map (fn [s] (str/split s #" ")))))
-
-(defn product [commands]
-  (->> (parse commands)
+       (map (fn [s] (str/split s #" ")))
        (map (fn [[c q]]
               (let [q (Integer. q)]
                 (case c
                   "forward" [q 0]
                   "up"      [0 (- 0 q)]
                   "down"    [0 q]
-                  [0 0]))))
+                  [0 0]))))))
+
+(defn product [commands]
+  (->> (parse commands)
        (reduce (fn [[ad aq] [d q]] [(+ ad d) (+ aq q)]) [0 0])
        (reduce *)))
 
 (defn product2 [commands]
   (->> (parse commands)
-       (map (fn [[c q]]
-              (let [q (Integer. q)]
-                (case c
-                  "forward" [q 0]
-                  "up"      [0 (- 0 q)]
-                  "down"    [0 q]
-                  [0 0]))))
        (reduce (fn [[ahoriz adepth aaim] [horiz aim]]
                  (let [caim (+ aaim aim)]
                    [(+ ahoriz horiz) (+ adepth (* caim horiz)) caim]))
